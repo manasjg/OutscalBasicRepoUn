5899e8698f4d3fdbf0a889c1e7e2dc7ed6b36c98
Manas Gangwani <manasjg1996@gmail.com>
2020-09-18 18:52:26 +0530

    Completed Mini Assignment 1

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Button button;
+    public string sceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(LoadScene);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("My first game awake function");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My first game start function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("F pressed down");
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("F pressed");
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            Debug.Log("F released");
+        }
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+    private void OnDestroy()
+    {
+        
+    }
+}
